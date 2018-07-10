7501c8e0df644dc452e56c44b69cf6ca3174acc9
Peter Züger <zueger.peter@icloud.com>
2018-07-10 10:09:35 +0200

    added assert message

--- src/assert.c
+++ src/assert.c
@@ -6,9 +6,17 @@
  */
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void __assert(const char* expr,const char* file,
               const char* line,const char* func){
-
+    fputs("assertion failed: ",stderr);
+    fputs(expr,stderr);
+    fputs("\n\t in file: ",stderr);
+    fputs(file,stderr);
+    fputs("\n\t on line: ",stderr);
+    fputs(line,stderr);
+    fputs("\n\t in function: ",stderr);
+    fputs(file,stderr);
     abort();
 }
