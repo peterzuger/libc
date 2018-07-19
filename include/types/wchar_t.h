e081bb91e66f75234d09b6fb7cd5231a991868ee
Peter Züger <zueger.peter@icloud.com>
2018-07-19 20:08:12 +0200

    wchar_t is a builtin type in C++

--- include/types/wchar_t.h
+++ include/types/wchar_t.h
@@ -7,11 +7,13 @@
 #ifndef __WCHAR_T_H__
 #define __WCHAR_T_H__
 
-#if defined(__WCHAR_TYPE__) || defined(DOXYGEN)
-# if !defined(__WCHAR_T_DEFINED__)
-#  define __WCHAR_T_DEFINED__
+#if !defined(__cplusplus)
+# if defined(__WCHAR_TYPE__) || defined(DOXYGEN)
+#  if !defined(__WCHAR_T_DEFINED__)
+#   define __WCHAR_T_DEFINED__
 typedef __WCHAR_TYPE__ wchar_t;
-# endif /* !defined(__WCHAR_T_TYPE_DEFINED__) */
-#endif /* defined(__WCHAR_TYPE__) || defined(DOXYGEN) */
+#  endif /* !defined(__WCHAR_T_TYPE_DEFINED__) */
+# endif /* defined(__WCHAR_TYPE__) || defined(DOXYGEN) */
+#endif /* !defined(__cplusplus) */
 
 #endif /* __WCHAR_T_H__ */
