8eea08e8343a108f85eeaf14f60eb9ce67ad5a78
Peter Züger <zueger.peter@icloud.com>
2018-07-22 21:42:33 +0200

    added strcmp() implementation

---
+++ src/string/strcmp.c
@@ -0,0 +1,16 @@
+/**
+ * @file   strcmp.c
+ * @author Peter Zueger
+ * @date   22.07.2018
+ * @brief  strcmp() implementation
+ */
+#include <string.h>
+
+int strcmp(const char* s1, const char* s2){
+    const unsigned char *l = (const unsigned char*)s1;
+    const unsigned char *r = (const unsigned char*)s2;
+
+    while( *l && *l == *r ) ++l, ++r;
+
+    return ( *l > *r ) - ( *r  > *l );
+}
