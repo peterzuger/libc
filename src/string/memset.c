e4f499eae2bfb910c09f7e6d7be9e9144bd76579
Peter Züger <zueger.peter@icloud.com>
2018-07-22 21:27:30 +0200

    added memset() implementation

---
+++ src/string/memset.c
@@ -0,0 +1,14 @@
+/**
+ * @file   memset.c
+ * @author Peter Zueger
+ * @date   22.07.2018
+ * @brief  memset() implementation
+ */
+#include <string.h>
+
+void* memset(void* s, int c, size_t n){
+    unsigned char* p = s;
+    while(n--)
+        *p++ = (unsigned char)c;
+    return s;
+}
