1e450a10fe64325106363a8512f7bd62dcc4e90c
Peter Züger <zueger.peter@icloud.com>
2018-07-22 21:35:23 +0200

    added memcmp() implementation

---
+++ src/string/memcmp.c
@@ -0,0 +1,18 @@
+/**
+ * @file   memcmp.c
+ * @author Peter Zueger
+ * @date   22.07.2018
+ * @brief  memcmp() implementation
+ */
+#include <string.h>
+
+int memcmp(const void* s1, const void* s2, size_t n){
+    const unsigned char *l = (const unsigned char*)s1;
+    const unsigned char *r = (const unsigned char*)s2;
+
+    while(n--){
+        if(*l++ != *r++)
+            return l[-1] < r[-1] ? -1 : 1;
+    }
+    return 0;
+}
