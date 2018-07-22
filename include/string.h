9756626c613ac6bbbdacc0be848d6912a7374bef
Peter Züger <zueger.peter@icloud.com>
2018-07-22 18:50:30 +0200

    added function prototypes to string.h

--- include/string.h
+++ include/string.h
@@ -7,6 +7,52 @@
 #ifndef __STRING_H__
 #define __STRING_H__
 
+#include <types/size_t.h>
+#include <macros/NULL.h>
 
+#if defined(__cplusplus)
+extern "C"{
+#endif /* defined(__cplusplus) */
+
+
+void* memcpy(void* __restrict__ s1, const void* __restrict__ s2, size_t n);
+void* memmove(void* s1, void* s2, size_t n);
+
+char* strcpy(char* __restrict__ s1, const char* __restrict__ s2);
+char* strncpy(char* __restrict__ s1, const char* __restrict__ s2, size_t n);
+
+char* strcat(char* __restrict__ s1, const char* __restrict__ s2);
+char* strncat(char* __restrict__ s1, const char* __restrict__ s2, size_t n);
+
+
+int memcmp(const void* s1, const void* s2, size_t n);
+int strcmp(const char* s1, const char* s2);
+
+int strcoll(const char* s1, const char* s2);
+int strncmp(const char* s1, const char* s2, size_t n);
+size_t strxfrm(char* __restrict__ s1, const char* __restrict__ s2, size_t n);
+
+
+void* memchr(const void* s, int c, size_t n);
+char* strchr(const char* s, int c);
+
+size_t strcspn(const char* s1, const char* s2);
+char* strrchr(const char* s, int c);
+char* strpbrk(const char* s1, const char* s2);
+char* strrchr(const char* s, int c);
+size_t strspn(const char* s1, const char* s2);
+char* strstr(const char* s1, const char* s2);
+char* strtok(char* __restrict__ s1, const char* __restrict__ s2);
+
+
+void* memset(void* s, int c, size_t n);
+char* strerror(int errnum);
+
+size_t strlen(const char* s);
+
+
+#if defined(__cplusplus)
+}
+#endif /* defined(__cplusplus) */
 
 #endif /* __STRING_H___ */
