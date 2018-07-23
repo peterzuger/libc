35153c824f62e4093912f0be67c0149ea6d81a24
Peter Züger <zueger.peter@icloud.com>
2018-07-23 19:04:37 +0200

    added feature test macros to stdalign.h

--- include/stdalign.h
+++ include/stdalign.h
@@ -13,6 +13,9 @@
 #define alignas _Alignas
 #define alignof _Alignof
 
+#define __alignas_is_defined 1
+#define __alignof_is_defined 1
+
 #endif /* !defined(__cplusplus) */
 
 #endif /* __STDALIGN_H__ */
