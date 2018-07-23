8f7cfcec8ae53a62d86d4e5b8f497d2db9ab0672
Peter Züger <zueger.peter@icloud.com>
2018-07-23 19:07:09 +0200

    added stdnoreturn.h with implementation

---
+++ include/stdnoreturn.h
@@ -0,0 +1,13 @@
+/**
+ * @file   stdnoreturn.h
+ * @author Peter Zueger
+ * @date   22.07.2018
+ * @note   part of the freestanding headers
+ * @brief  noreturn specifier
+ */
+#ifndef __STDNORETURN_H__
+#define __STDNORETURN_H__
+
+#define noreturn _Noreturn
+
+#endif /* __STDNORETURN_H__ */
