47cbeea1715e683a50943d9bcbb7f48f4e38b313
Peter Züger <zueger.peter@icloud.com>
2018-07-25 00:23:18 +0200

    added signal types macros and prototypes

--- include/signal.h
+++ include/signal.h
@@ -7,6 +7,35 @@
 #ifndef __SIGNAL_H__
 #define __SIGNAL_H__
 
+#include <types/sig_atomic_t.h>
 
+typedef void (*__sighandler_t)(int);
+#define SIG_DFL	(( __sighandler_t)0)	/* default signal handling */
+#define SIG_IGN	(( __sighandler_t)1)	/* ignore signal */
+#define SIG_ERR	(( __sighandler_t)-1)	/* error return from signal */
+
+
+#define SIGABRT  1
+#define SIGFPE   2
+#define SIGILL   3
+#define SIGINT   4
+#define SIGSEGV  5
+#define SIGTERM  6
+
+#if defined(__cplusplus)
+extern "C"{
+#endif /* defined(__cplusplus) */
+
+
+// Specify signal handling
+void (*signal(int sig, void (*func)(int)))(int);
+
+// Send signal
+int raise(int sig);
+
+
+#if defined(__cplusplus)
+}
+#endif /* defined(__cplusplus) */
 
 #endif /* __SIGNAL_H___ */
---
+++ include/types/sig_atomic_t.h
@@ -0,0 +1,19 @@
+/**
+ * @file   sig_atomic_t.h
+ * @author Peter Zueger
+ * @date   24.07.2018
+ * @brief  atomic accessible type
+ */
+#ifndef __SIG_ATOMIC_T_H__
+#define __SIG_ATOMIC_T_H__
+
+#if defined(__UINT32_TYPE__) || defined(DOXYGEN)
+# if !defined(__SIG_ATOMIC_T_DEFINED__)
+#  define __SIG_ATOMIC_T_DEFINED__
+#  define SIG_ATOMIC_T_MIN (0)
+#  define SIG_ATOMIC_T_MAX (__UINT32_MAX__)
+typedef __UINT32_TYPE__ sig_atomic_t;
+# endif /* !defined(__SIG_ATOMIC_T_DEFINED__) */
+#endif /* defined(__UINT32_TYPE__) || defined(DOXYGEN) */
+
+#endif /* __SIG_ATOMIC_T_H__ */
