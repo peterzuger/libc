6594cb8c03bafe475f8e6cfba1ba1ea91811be77
Peter Züger <zueger.peter@icloud.com>
2018-07-25 20:27:09 +0200

    added strspn() implementation

---
+++ src/string/strspn.c
@@ -0,0 +1,29 @@
+/**
+ * @file   strcspn.c
+ * @author Peter Zueger
+ * @date   25.07.2018
+ * @brief  lengt of string only out of span
+ */
+#include <string.h>
+
+size_t strspn(const char* s1, const char* s2){
+    const unsigned char* l = (const unsigned char*)s1;
+    const unsigned char* r = (const unsigned char*)s2;
+    const unsigned char* p;
+    size_t n = 0;
+    int f = 0;
+
+    while(*l){
+        for(p = r;*p;p++)
+            if(*l == *p){
+                f = 1;
+                break;
+            }
+        if(!f)
+            return n;
+        f = 0;
+        n++;
+        l++;
+    }
+    return n;
+}
