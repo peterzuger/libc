30a5cb44561f73461e05f3a5ab73cc508f6304b5
Peter Züger <zueger.peter@icloud.com>
2018-07-25 20:24:14 +0200

    added memchr() implementation

---
+++ src/string/memchr.c
@@ -0,0 +1,15 @@
+/**
+ * @file   memchr.c
+ * @author Peter Zueger
+ * @date   25.07.2018
+ * @brief  find character in memory
+ */
+#include <string.h>
+
+void* memchr(const void* s, int c, size_t n){
+    unsigned char* p = (unsigned char*)s;
+    while(n--)
+        if(*p++ == (unsigned char)c)
+            return p-1;
+    return NULL;
+}
