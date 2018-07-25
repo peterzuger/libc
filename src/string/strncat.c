1b2e9ffaee4cf3c134d1836f007b4b1e449df42d
Peter Züger <zueger.peter@icloud.com>
2018-07-25 20:22:50 +0200

    added strncat() implementation

---
+++ src/string/strncat.c
@@ -0,0 +1,18 @@
+/**
+ * @file   strncat.c
+ * @author Peter Zueger
+ * @date   25.07.2018
+ * @brief  concatenate strings
+ */
+#include <string.h>
+
+char* strncat(char* __restrict__ s1, const char* __restrict__ s2, size_t n){
+    char* p = s1;
+
+    while(*s1++);
+    while(n-- && *s2)
+        *s1++ = *s2++;
+    *s1 = '\0';
+
+    return p;
+}
