bf3df8ceb03ce28387c97eaba78b4dedebfce940
Peter Züger <zueger.peter@icloud.com>
2018-07-25 20:26:25 +0200

    added strpbrk() implementation

---
+++ src/string/strpbrk.c
@@ -0,0 +1,21 @@
+/**
+ * @file   strpbrk.c
+ * @author Peter Zueger
+ * @date   25.07.2018
+ * @brief  pointer to first occurence of a span in string
+ */
+#include <string.h>
+
+char* strpbrk(const char* s1, const char* s2){
+    const unsigned char* l = (const unsigned char*)s1;
+    const unsigned char* r = (const unsigned char*)s2;
+    const unsigned char* p;
+
+    while(*l){
+        for(p = r;*p;p++)
+            if(*l == *p)
+                return (char*)l;
+        l++;
+    }
+    return NULL;
+}
