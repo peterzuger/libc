e54c0abffc5e5cf0bb368e1adc07322aaf1b0486
Peter Züger <zueger.peter@icloud.com>
2018-07-25 20:19:37 +0200

    remove duplicate functions and group functions

--- include/string.h
+++ include/string.h
@@ -15,16 +15,20 @@ extern "C"{
 #endif /* defined(__cplusplus) */
 
 
+// Copying functions
 void* memcpy(void* __restrict__ s1, const void* __restrict__ s2, size_t n);
 void* memmove(void* s1, void* s2, size_t n);
 
 char* strcpy(char* __restrict__ s1, const char* __restrict__ s2);
 char* strncpy(char* __restrict__ s1, const char* __restrict__ s2, size_t n);
 
+
+// Concatenation functions
 char* strcat(char* __restrict__ s1, const char* __restrict__ s2);
 char* strncat(char* __restrict__ s1, const char* __restrict__ s2, size_t n);
 
 
+// Comparison functions
 int memcmp(const void* s1, const void* s2, size_t n);
 int strcmp(const char* s1, const char* s2);
 
@@ -33,11 +37,11 @@ int strncmp(const char* s1, const char* s2, size_t n);
 size_t strxfrm(char* __restrict__ s1, const char* __restrict__ s2, size_t n);
 
 
+// Search functions
 void* memchr(const void* s, int c, size_t n);
 char* strchr(const char* s, int c);
 
 size_t strcspn(const char* s1, const char* s2);
-char* strrchr(const char* s, int c);
 char* strpbrk(const char* s1, const char* s2);
 char* strrchr(const char* s, int c);
 size_t strspn(const char* s1, const char* s2);
@@ -45,6 +49,7 @@ char* strstr(const char* s1, const char* s2);
 char* strtok(char* __restrict__ s1, const char* __restrict__ s2);
 
 
+// Miscellaneous functions
 void* memset(void* s, int c, size_t n);
 char* strerror(int errnum);
 
