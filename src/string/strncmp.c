40cd47a885ec86abca4800f0ef0249e17ca1523e
Peter Züger <zueger.peter@icloud.com>
2018-07-25 20:23:36 +0200

    added strncmp() implementation

---
+++ src/string/strncmp.c
@@ -0,0 +1,20 @@
+/**
+ * @file   strncmp.c
+ * @author Peter Zueger
+ * @date   25.07.2018
+ * @brief  compare up to n characters
+ */
+#include <string.h>
+
+int strncmp(const char* s1, const char* s2, size_t n){
+    const unsigned char* l = (const unsigned char*)s1;
+    const unsigned char* r = (const unsigned char*)s2;
+
+    while(n--){
+        if((*l != *r) || (!*l))
+            return ( *l > *r ) - ( *r  > *l );
+        l++;
+        r++;
+    }
+    return 0;
+}
