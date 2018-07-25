835d508fb6e2f28548f2fdfc14f424887fba6b3b
Peter Züger <zueger.peter@icloud.com>
2018-07-25 20:21:35 +0200

    added strncpy() implementation

---
+++ src/string/strncpy.c
@@ -0,0 +1,13 @@
+/**
+ * @file   strncpy.c
+ * @author Peter Zueger
+ * @date   25.07.2018
+ * @brief  copy n bytes in string
+ */
+#include <string.h>
+
+char* strncpy(char* __restrict__ s1, const char* __restrict__ s2, size_t n){
+    while(n--)
+        s1[n] = s2[n];
+    return s1;
+}
