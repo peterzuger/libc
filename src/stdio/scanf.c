816f3892061750d582d3e88b86993450b1c65c9f
Peter Züger <zueger.peter@icloud.com>
2018-07-26 19:07:27 +0200

    added scanf() implementation

---
+++ src/stdio/scanf.c
@@ -0,0 +1,19 @@
+/**
+ * @file   scanf.c
+ * @author Peter Zueger
+ * @date   26.07.2018
+ * @brief  scanf() implementation
+ */
+#include <stdio.h>
+#include <stdarg.h>
+
+int scanf(const char* __restrict__ format, ...){
+    va_list arg;
+    int r;
+
+    va_start(arg, format);
+    r = vscanf(format, arg);
+    va_end(arg);
+
+    return r;
+}
