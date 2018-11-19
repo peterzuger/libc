205ae73bc2fda956b8edbda5bd42d731e8696547
Peter Züger <zueger.peter@icloud.com>
2018-11-19 08:45:36 +0100

    added fgets implementation

---
+++ src/stdio/fgets.c
@@ -0,0 +1,19 @@
+/**
+ * @file   fgets.c
+ * @author Peter Zueger
+ * @date   14.11.2018
+ * @brief  fgets() implementation
+ */
+#include <stdio.h>
+
+char *fgets(char* __restrict__ s, int n, FILE* __restrict__ stream){
+    for(int m = 0; m < n; m++){
+        int c = fgetc(stream);
+        if(c == EOF)
+            return NULL;
+        s[m] = (char)c;
+        if(c == '\n')
+            return s;
+    }
+    return s;
+}
