fe2846e37c6bfa03947779c9d97e466c17e1dd22
Peter Züger <zueger.peter@icloud.com>
2019-05-04 12:58:15 +0200

    added setjmp header

--- include/setjmp.h
+++ include/setjmp.h
@@ -7,6 +7,24 @@
 #ifndef __SETJMP_H__
 #define __SETJMP_H__
 
+typedef struct{}jmp_buf;
 
+
+// 7.13.1 Save calling environment
+#define setjmp(jmp_buf) (0)
+
+
+#if defined(__cplusplus)
+extern "C"{
+#endif /* defined(__cplusplus) */
+
+
+// 7.13.2 Restore calling environment
+__attribute__((noreturn)) void longjmp(jmp_buf env, int val);
+
+
+#if defined(__cplusplus)
+}
+#endif /* defined(__cplusplus) */
 
 #endif /* __SETJMP_H___ */
