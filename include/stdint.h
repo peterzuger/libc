254a8e5cdbc6e354b89f84660655167dc58a86d9
Peter Züger <zueger.peter@icloud.com>
2019-05-13 11:12:04 +0200

    changed all includes to use <> instead of ""

--- include/stdint.h
+++ include/stdint.h
@@ -8,38 +8,38 @@
 #ifndef __STDINT_H__
 #define __STDINT_H__
 
-#include "types/int64_t.h"
-#include "types/uint64_t.h"
-#include "types/int_least64_t.h"
-#include "types/uint_least64_t.h"
-#include "types/int_fast64_t.h"
-#include "types/uint_fast64_t.h"
+#include <types/int64_t.h>
+#include <types/uint64_t.h>
+#include <types/int_least64_t.h>
+#include <types/uint_least64_t.h>
+#include <types/int_fast64_t.h>
+#include <types/uint_fast64_t.h>
 
-#include "types/int32_t.h"
-#include "types/uint32_t.h"
-#include "types/int_least32_t.h"
-#include "types/uint_least32_t.h"
-#include "types/int_fast32_t.h"
-#include "types/uint_fast32_t.h"
+#include <types/int32_t.h>
+#include <types/uint32_t.h>
+#include <types/int_least32_t.h>
+#include <types/uint_least32_t.h>
+#include <types/int_fast32_t.h>
+#include <types/uint_fast32_t.h>
 
-#include "types/int16_t.h"
-#include "types/uint16_t.h"
-#include "types/int_least16_t.h"
-#include "types/uint_least16_t.h"
-#include "types/int_fast16_t.h"
-#include "types/uint_fast16_t.h"
+#include <types/int16_t.h>
+#include <types/uint16_t.h>
+#include <types/int_least16_t.h>
+#include <types/uint_least16_t.h>
+#include <types/int_fast16_t.h>
+#include <types/uint_fast16_t.h>
 
-#include "types/int8_t.h"
-#include "types/uint8_t.h"
-#include "types/int_least8_t.h"
-#include "types/uint_least8_t.h"
-#include "types/int_fast8_t.h"
-#include "types/uint_fast8_t.h"
+#include <types/int8_t.h>
+#include <types/uint8_t.h>
+#include <types/int_least8_t.h>
+#include <types/uint_least8_t.h>
+#include <types/int_fast8_t.h>
+#include <types/uint_fast8_t.h>
 
-#include "types/intptr_t.h"
-#include "types/uintptr_t.h"
+#include <types/intptr_t.h>
+#include <types/uintptr_t.h>
 
-#include "types/intmax_t.h"
-#include "types/uintmax_t.h"
+#include <types/intmax_t.h>
+#include <types/uintmax_t.h>
 
 #endif /* __STDINT_H__ */
