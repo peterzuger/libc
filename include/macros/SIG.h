7aa1616aee947a18e7bfff8a4f61ae00fe1d43fa
Peter Züger <zueger.peter@icloud.com>
2019-10-15 20:55:57 +0200

    moved signal macros to macros/

---
+++ include/macros/SIG.h
@@ -0,0 +1,35 @@
+/**
+ * @file   SIG.h
+ * @author Peter Züger
+ * @date   09.10.2019
+ * @brief  signals
+ *
+ * This file is part of libc (https://gitlab.com/peterzuger/libc).
+ * Copyright (c) 2019 Peter Züger.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __MACROS_SIG_H__
+#define __MACROS_SIG_H__
+
+#define SIGABRT  (0) /* abnormal termination,                             */
+                     /* such as is initiated by the abort function        */
+#define SIGFPE   (1) /* an erroneous arithmetic operation, such as        */
+                     /* zero divide or an operation resulting in overflow */
+#define SIGILL   (2) /* detection of an invalid function image,           */
+                     /* such as an invalid instruction                    */
+#define SIGINT   (3) /* receipt of an interactive attention signal        */
+#define SIGSEGV  (4) /* an invalid access to storage                      */
+#define SIGTERM  (5) /* a termination request sent to the program         */
+
+#endif /* __MACROS_SIG_H__ */
--- include/macros/SIG_IGN.h
+++ include/macros/SIG_IGN.h
@@ -1,8 +1,8 @@
 /**
- * @file   include/signal.h
+ * @file   include/macros/SIG_IGN.h
  * @author Peter Züger
- * @date   07.02.2018
- * @brief  Signal handling
+ * @date   09.10.2019
+ * @brief  ignore signal
  *
  * This file is part of libc (https://gitlab.com/peterzuger/libc).
  * Copyright (c) 2019 Peter Züger.
@@ -19,31 +19,21 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef __SIGNAL_H__
-#define __SIGNAL_H__
-
-#include <types/sig_atomic_t.h>
-
-#include <macros/SIG_DFL.h>
-#include <macros/SIG_IGN.h>
-#include <macros/SIG_ERR.h>
-#include <macros/SIG.h>
+#ifndef __MACROS_SIG_IGN_H__
+#define __MACROS_SIG_IGN_H__
 
 #if defined(__cplusplus)
 extern "C"{
 #endif /* defined(__cplusplus) */
 
 
-// 7.14.1 Specify signal handling
-void (*signal(int sig, void (*func)(int)))(int);
-
-
-// 7.14.2 Send signal
-int raise(int sig);
+void __signal_handler_ignore(int);
 
 
 #if defined(__cplusplus)
 }
 #endif /* defined(__cplusplus) */
 
-#endif /* __SIGNAL_H___ */
+#define SIG_IGN (__signal_handler_ignore)
+
+#endif /* __MACROS_SIG_IGN_H__ */
