1a373a21dc57f69b7e1b36499cdf948a827cbbae
Peter Züger <zueger.peter@icloud.com>
2019-11-05 19:50:51 +0100

    implemented _Exit(int)

---
+++ src/stdlib/_Exit.c
@@ -0,0 +1,28 @@
+/**
+ * @file   libc/src/stdlib/_Exit.c
+ * @author Peter Züger
+ * @date   18.10.2019
+ * @brief  7.22.4.5  The _Exit function
+ *
+ * This file is part of libc (https://gitlab.com/peterzuger/libc).
+ * Copyright (c) 2019 Peter Züger.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdlib.h>
+
+__attribute__((noreturn)) void _Exit(int status){
+    (void)status;
+    abort();
+    __builtin_unreachable();
+}
